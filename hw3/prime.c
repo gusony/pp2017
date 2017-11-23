0ff78e22d90a41f192c9d78ab74e0f3acb755821
gusony <junhan@g2.nctu.edu.tw>
2017-11-24 00:15:50 +0800

    test rank and size

--- hw3/prime.c
+++ hw3/prime.c
@@ -4,8 +4,8 @@
 #include <math.h>
 #include <time.h>
 
-int isprime(int n) {
-  int i,squareroot;
+int isprime(long long int n) {
+  long long int i,squareroot;
   if (n>10) {
     squareroot = (int) sqrt(n);
     for (i=3; i<=squareroot; i=i+2)
@@ -17,29 +17,45 @@ int isprime(int n) {
     return 0;
 }
 
-int main(int argc, char *argv[])
-{
-  long long int pc,       /* prime counter */
-                foundone; /* most recent prime found */
-  long long int n, limit;
-  time_t start_time, end_time;
-  
-  sscanf(argv[1],"%llu",&limit);	
-  printf("Starting. Numbers to be scanned= %lld\n",limit);
+int main(int argc, char *argv[]){
+	long long int pc,       /* prime counter */
+                  foundone; /* most recent prime found */
+	long long int n, limit,avg;
+	time_t start_time, end_time;
+	int size=-1,rank=-1;
 
-  pc=4;     /* Assume (2,3,5,7) are counted here */
-  start_time = clock();
-  MPI_Init(&argc, &argv);
-  for (n=11; n<=limit; n=n+2) {
-    if (isprime(n)) {
-      pc++;
-      foundone = n;
-    }			
-  }
-  MPI_Finalize();
-  end_time = clock();
-  printf("use %fs\n",(end_time - start_time) / CLOCKS_PER_SEC );
-  printf("Done. Largest prime is %d Total primes %d\n",foundone,pc);
+	sscanf(argv[1],"%llu",&limit);
+	printf("Starting. Numbers to be scanned= %lld\n",limit);
 
-  return 0;
-} 
+	pc=4;     /* Assume (2,3,5,7) are counted here */
+	start_time = clock();
+	MPI_Init(&argc, &argv);
+	MPI_Comm_size(MPI_COMM_WORLD, &size);
+	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+	avg = (long long int)(limit/size);
+
+	if(rank == size-1){//least process
+	    for (n = (avg*rank)+1; n <= limit; n=n+2){
+	        if (isprime(n)) {
+	            pc++;
+	            foundone = n;
+	        }
+	    }
+	}
+	else{
+		for (n = avg*rank+1 ; n <= avg*(rank+1); n=n+2){
+			if (isprime(n)) {
+				pc++;
+				foundone = n;
+			}
+		}
+	}
+
+	MPI_Finalize();
+	end_time = clock();
+	printf("rank%d, use %.6fs, ",rank,(end_time - start_time)/(float)CLOCKS_PER_SEC);
+	printf("Done. Largest prime is %d Total primes %d\n",foundone,pc);
+
+	return 0;
+}
+
