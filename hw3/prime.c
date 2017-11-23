83c1b89e8e98b1a9308b7953c1b7433f942cdc41
gusony <junhan@g2.nctu.edu.tw>
2017-11-23 09:54:15 +0800

    change pc and foundone to 'long long int' tye

--- hw3/prime.c
+++ hw3/prime.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
-
+#include <time.h>
 
 int isprime(int n) {
   int i,squareroot;
@@ -19,21 +19,26 @@ int isprime(int n) {
 
 int main(int argc, char *argv[])
 {
-  int pc,       /* prime counter */
-      foundone; /* most recent prime found */
+  long long int pc,       /* prime counter */
+                foundone; /* most recent prime found */
   long long int n, limit;
-
+  time_t start_time, end_time;
+  
   sscanf(argv[1],"%llu",&limit);	
   printf("Starting. Numbers to be scanned= %lld\n",limit);
 
   pc=4;     /* Assume (2,3,5,7) are counted here */
-
+  start_time = clock();
+  MPI_Init(&argc, &argv);
   for (n=11; n<=limit; n=n+2) {
     if (isprime(n)) {
       pc++;
       foundone = n;
     }			
   }
+  MPI_Finalize();
+  end_time = clock();
+  printf("use %fs\n",(end_time - start_time) / CLOCKS_PER_SEC );
   printf("Done. Largest prime is %d Total primes %d\n",foundone,pc);
 
   return 0;
