c9ea665791c9d55ebadec46c2468e5a3773f4b34
gusony <junhan@g2.nctu.edu.tw>
2017-11-24 19:24:25 +0800

    prime finish

--- hw3/b_integrate.c
+++ hw3/b_integrate.c
@@ -3,31 +3,26 @@
 
 #define PI 3.1415926535
 
-int main(int argc, char **argv)
+int main(int argc, char **argv) 
 {
   long long i, num_intervals;
-  double rect_width, area, sum, x_middle;
-  int size = -1, rank = -1;
+  double rect_width, area, sum, x_middle; 
 
   sscanf(argv[1],"%llu",&num_intervals);
-
-  MPI_Init(&argc, &argv);
-  MPI_Comm_size(MPI_COMM_WORLD, &size);
-  MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 
   rect_width = PI / num_intervals;
 
   sum = 0;
   for(i = 1; i < num_intervals + 1; i++) {
 
-    /* find the middle of the interval on the X-axis. */
+    /* find the middle of the interval on the X-axis. */ 
 
     x_middle = (i - 0.5) * rect_width;
-    area = sin(x_middle) * rect_width;
+    area = sin(x_middle) * rect_width; 
     sum = sum + area;
-  }
+  } 
 
   printf("The total area is: %f\n", (float)sum);
 
   return 0;
-}
+}   
--- hw3/prime.c
+++ hw3/prime.c
@@ -3,6 +3,7 @@
 #include <stdlib.h>
 #include <math.h>
 #include <time.h>
+#include <string.h>
 
 int isprime(long long int n) {
   long long int i,squareroot;
@@ -18,43 +19,64 @@ int isprime(long long int n) {
 }
 
 int main(int argc, char *argv[]){
-	long long int pc,       /* prime counter */
-                  foundone; /* most recent prime found */
-	long long int n, limit,avg;
+	long long int pc,       // prime counter
+                  foundone; // most recent prime found
+	long long int temp_pc,       // to receive data from other thread
+                  temp_foundone; // to receive data from other thread
+    long long int n, limit,avg,up_bound, recv_buf;
 	time_t start_time, end_time;
 	int size=-1,rank=-1;
 
 	sscanf(argv[1],"%llu",&limit);
-	printf("Starting. Numbers to be scanned= %lld\n",limit);
-
+	if(rank == size-1)
+		printf("Starting. Numbers to be scanned= %lld\n",limit);
 	pc=4;     /* Assume (2,3,5,7) are counted here */
 	start_time = clock();
+	int tag = 0;
+	char *message = malloc(sizeof(char)*100);
+
+    MPI_Status status;
 	MPI_Init(&argc, &argv);
 	MPI_Comm_size(MPI_COMM_WORLD, &size);
 	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-	avg = (long long int)(limit/size);
 
-	if(rank == size-1){//least process
-	    for (n = (avg*rank)+1; n <= limit; n=n+2){
-	        if (isprime(n)) {
-	            pc++;
-	            foundone = n;
-	        }
-	    }
+	avg = (long long int)(limit/size);
+	if(rank == size-1)   //least process
+		up_bound = limit;
+	else
+		up_bound = avg*(rank+1);
+
+
+	if((avg*rank+1)%2 == 0)//start number must be odd number
+		n = avg*rank+2;
+	else
+	    n = avg*rank+1;
+
+	for (n ; n<=up_bound; n=n+2){
+        if (isprime(n)) {
+            pc++;
+            foundone = n;
+        }
+    }
+
+
+    if(rank == size-1){
+    	for(int i=0; i<rank; i++){
+    		MPI_Recv(&recv_buf, 1, MPI_INT, i, tag, MPI_COMM_WORLD, &status);
+
+    		pc += recv_buf;
+    	}
+    	end_time = clock();
+		//printf("use %.6fs, ",(end_time - start_time)/(float)CLOCKS_PER_SEC);
+		printf("Done. Largest prime is %d Total primes %d\n",foundone,pc);
 	}
 	else{
-		for (n = avg*rank+1 ; n <= avg*(rank+1); n=n+2){
-			if (isprime(n)) {
-				pc++;
-				foundone = n;
-			}
-		}
+		MPI_Send(&pc, 1, MPI_INT, size-1, tag, MPI_COMM_WORLD);
 	}
 
-	MPI_Finalize();
-	end_time = clock();
-	printf("rank%d, use %.6fs, ",rank,(end_time - start_time)/(float)CLOCKS_PER_SEC);
-	printf("Done. Largest prime is %d Total primes %d\n",foundone,pc);
+    MPI_Finalize();
+
+
 
 	return 0;
 }
